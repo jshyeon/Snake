88dece0b7129576c918bd787b5968da7e6bda8c5
LAPTOP-986IB660\jshye <jshyeon550@gmail.com>
2019-01-08 15:07:49 +0900

    what's going on??

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
+//#include <allegro5/allegro_color.h>
+
+ALLEGRO_COLOR color_green = al_map_rgb(0,0xff,0);
 
 int main(int argc, char **argv) {
 
 	ALLEGRO_DISPLAY *display = NULL;
 
-	if (!al_init() || !al_init_primitives_addon()) {
+	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+	if (!al_init_primitives_addon()) {
+		fprintf(stderr, "failed to initialize allegro primitives addon!\n");
 		return -1;
 	}
 	display = al_create_display(600, 600);
@@ -17,8 +24,10 @@ int main(int argc, char **argv) {
 	}
 
 	al_clear_to_color(al_map_rgb(0xff, 0xff, 0xff));
-
+	/* green filled rectangle */
+	al_draw_filled_rectangle(10, 10, 60, 60, color_green);
 	al_flip_display();
+
 	al_rest(3.0);
 	al_destroy_display(display);
 
