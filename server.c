69b4569969339bb95c5e6f6869793e1fa9bd39b5
JHmini <hminj150311@gmail.com>
2019-01-21 10:28:31 +0900

    server

--- server.c
+++ server.c
@@ -11,6 +11,8 @@
 #include <pthread.h>
 #define MAXBUF 256
 
+short lock;
+
 struct args {
 	int client_sockfd;
 	int server_sockfd;
@@ -20,30 +22,44 @@ struct args {
 
 void *p_accept(void *argument) {
 	struct args arg = *(struct args *) argument;
-printf("In the p_accept\n");
-	arg.client_sockfd = accept(arg.server_sockfd, arg.clientaddr, arg.client_len);	// !@#$
-printf("client_sockfd = %d\n", arg.client_sockfd);
-	return argument;	// I will not use this return
+	char buf[256];
+	int n;
+
+	// pthread_detach(pthead_self()); // make this thread independent
+	arg.client_sockfd = accept(arg.server_sockfd, arg.clientaddr, arg.client_len);
+	printf("client_sockfd = %d is connected\n", arg.client_sockfd);
+	lock = 0;
+	while(1) {
+		memset(buf, 0x00, 256);
+		read(arg.client_sockfd, buf, 255);	// 255? 256?
+		
+		n = strcmp(buf, "/quit");	// this is the problem;
+		if (!n) {break;}		// this is the problem;
+		
+		memset(buf, 0x00, MAXBUF);
+		strcpy(buf, "Player Connected!\n");
+		write(arg.client_sockfd, buf, MAXBUF);
+	}
+	close(arg.client_sockfd);
+	pthread_exit((void *) 0);
+	return argument;
 }
 
 int main(int argc, char **argv)
 {
-    int server_sockfd, client1_sockfd, client2_sockfd;
-    int client_len;
-    int n;
-    int def1, def2;
+    int server_sockfd, client1_sockfd, client2_sockfd, client_len;
+    int n, status1, status2, def1, def2;
     char buf[MAXBUF];
     struct sockaddr_in clientaddr, serveraddr;
-    struct args args_p1;
-    struct args args_p2;
+    struct args args_p1, args_p2;
     pthread_t p1 = -10, p2 = -10;
     
     client_len = sizeof(clientaddr);
-
     if ((server_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         perror("socket error : ");
         exit(0);
     }
+
     bzero(&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -59,29 +75,16 @@ int main(int argc, char **argv)
 		args_p1.clientaddr = &clientaddr;	// what warning?
 		args_p1.client_len = &client_len;
 
-		def1 = args_p1.client_sockfd;
+		lock = 1;
 
 		n = pthread_create(&p1, NULL, p_accept, &args_p1);
-		// automatically p1 is saved
 		
-		for(n = 0; n < 3; n++) {	// need some codition whether it is connected
-			sleep(1);
+		while(lock == 1) {
+			sleep(3);
+			printf("player1 no connection... \n");
 		}
-		if (args_p1.client_sockfd == def1) {
-			printf("accept error\n");
-			exit(0);
-			//continue;
-		}
-		else {
-			memset(buf, 0x00, MAXBUF);
-			strcpy(buf, "Player1 Connected!\n");
-			write(args_p1.client_sockfd, buf, MAXBUF);
-		}
-
-		memset(buf, 0x00, MAXBUF);
-		strcpy(buf, "wait for Player2\n");
-		write(args_p1.client_sockfd, buf, MAXBUF);
-
+		printf("player1 connected\n");
+		
 		continue;
 	} 
 	else if(p1 != -10 && p2 == -10) {
@@ -90,39 +93,25 @@ int main(int argc, char **argv)
 		args_p2.clientaddr = &clientaddr;	// what warning?
 		args_p2.client_len = &client_len;
 
-		def2 = args_p2.client_sockfd;
+		lock = 1;
 
 		n = pthread_create(&p2, NULL, p_accept, &args_p2);
 
-		for(n = 0; n < 3; n++) {	// need some codition whether it is connected
-			sleep(1);
+		while(lock == 1) {
+			sleep(3);
+			printf("player2 no connection... \n");
 		}
-		if (args_p2.client_sockfd == def2) {
-			printf("accept error\n");
-			exit(0);
-			//continue;	maybe need some close()
-		}
-		else {
-			memset(buf, 0x00, MAXBUF);
-			strcpy(buf, "Player2 Connected!\n");
-			write(args_p1.client_sockfd, buf, MAXBUF);
-		}
-
+		printf("player2 connected\n");
+		
 		continue;
 	} 
-	else {
-		// n = read(client_sockfd, buf, MAXBUF);
-		// write(client1_sockfd, buf, MAXBUF);
+	else {}
 
-		/// network communication ///
-
-	}
-		//pthread_join(thread_t, (void **)&status);
-		//printf("Thread End %d\n", status);
-		close(client1_sockfd);
-		close(client2_sockfd);
-		close(server_sockfd);
-		break;
+	pthread_join(p1, (void **)&status1);
+	pthread_join(p2, (void **)&status2);
+	printf("Thread End %d %d\n", status1, status2);
+	close(server_sockfd);
+	break;
     }
     return 0;
 }
