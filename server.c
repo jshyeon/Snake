26ba244bfa48959ad31e216895b935d9bbd4bdd3
JHmini <hminj150311@gmail.com>
2019-01-21 13:42:55 +0900

    communication server

--- server.c
+++ server.c
@@ -12,6 +12,8 @@
 #define MAXBUF 256
 
 short lock;
+int client1_sockfd, 
+    client2_sockfd;
 
 struct args {
 	int client_sockfd;
@@ -24,21 +26,45 @@ void *p_accept(void *argument) {
 	struct args arg = *(struct args *) argument;
 	char buf[256];
 	int n;
+	short player_name;
 
 	// pthread_detach(pthead_self()); // make this thread independent
 	arg.client_sockfd = accept(arg.server_sockfd, arg.clientaddr, arg.client_len);
 	printf("client_sockfd = %d is connected\n", arg.client_sockfd);
 	lock = 0;
+
+	if (client1_sockfd == -10) {
+		client1_sockfd == arg.client_sockfd;
+		player_name = 1;
+	} else if (client1_sockfd != -10 && client2_sockfd == -10) {
+		client2_sockfd == arg.client_sockfd;
+		player_name = 2;
+	} else {}
+
 	while(1) {
-		memset(buf, 0x00, 256);
-		read(arg.client_sockfd, buf, 255);	// 255? 256?
-		
-		n = strcmp(buf, "/quit");	// this is the problem;
-		if (!n) {break;}		// this is the problem;
-		
-		memset(buf, 0x00, MAXBUF);
-		strcpy(buf, "Player Connected!\n");
-		write(arg.client_sockfd, buf, MAXBUF);
+		if (buf[0] != '$') {	/*case1*/
+			memset(buf, 0x00, 256);
+			read(arg.client_sockfd, buf, 255);	// 255? 256?
+			
+			n = strcmp(buf, "/quit");	// this is the problem;
+			if (!n) {break;}		// this is the problem;
+			
+			memset(buf, 0x00, MAXBUF);
+			strcpy(buf, "Player Connected!\n");
+			write(arg.client_sockfd, buf, MAXBUF);
+		} else {		/*case2*/
+			memset(buf, 0x00, 256);
+			read(arg.client_sockfd, buf, 255);	// 255? 256?
+			if (player_name == 1) {
+				write(client2_sockfd, buf, MAXBUF);
+			}
+			else {
+				write(client1_sockfd, buf, MAXBUF);
+			}
+			
+			n = strcmp(buf, "/quit");	// this is the problem;
+			if (!n) {break;}		// this is the problem;
+		}
 	}
 	close(arg.client_sockfd);
 	pthread_exit((void *) 0);
@@ -48,12 +74,14 @@ void *p_accept(void *argument) {
 int main(int argc, char **argv)
 {
     int server_sockfd, client1_sockfd, client2_sockfd, client_len;
-    int n, status1, status2, def1, def2;
+    int n, status1, status2;
     char buf[MAXBUF];
     struct sockaddr_in clientaddr, serveraddr;
     struct args args_p1, args_p2;
     pthread_t p1 = -10, p2 = -10;
-    
+    client1_sockfd = -10;
+    client2_sockfd = -10;
+
     client_len = sizeof(clientaddr);
     if ((server_sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         perror("socket error : ");
@@ -109,6 +137,8 @@ int main(int argc, char **argv)
 
 	pthread_join(p1, (void **)&status1);
 	pthread_join(p2, (void **)&status2);
+
+printf("client1_sockfd = %d / client2_socfd = %d\n", client1_sockfd, client2_sockfd);
 	printf("Thread End %d %d\n", status1, status2);
 	close(server_sockfd);
 	break;
